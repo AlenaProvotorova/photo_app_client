631186680844294704aec86f2d9b556bcbfb6897
Alena Provotorova <lunichkina.alena@gmail.com>
2025-02-19 16:03:53 +0300

    upload files

--- lib/presentation/folders_storage/pages/folder_item/bloc/files_event.dart
+++ lib/presentation/folders_storage/pages/folder_item/bloc/files_event.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:photo_app/data/image_picker/models/image_data.dart';
+// import 'package:photo_app/data/image_picker/models/image_data.dart';
 
 abstract class FilesEvent {}
 
 class LoadFiles extends FilesEvent {}
 
-class AddImages extends FilesEvent {
-  final List<ImageData> images;
-  final BuildContext context;
+// class AddImages extends FilesEvent {
+//   final List<ImageData> images;
+//   final BuildContext context;
 
-  AddImages(this.images, this.context);
-}
+//   AddImages(this.images, this.context);
+// }
 
 class UploadImage extends FilesEvent {
   final ImageData image;
   final BuildContext context;
+  // final String folderId;
 
-  UploadImage(this.image, this.context);
+  UploadImage({
+    required this.image,
+    required this.context,
+    // required this.folderId,
+  });
 }
