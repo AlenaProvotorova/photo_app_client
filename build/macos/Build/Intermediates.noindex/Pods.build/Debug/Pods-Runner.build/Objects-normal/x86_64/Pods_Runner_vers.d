20c034993245b227b3614abcc544b273eef6b38f
Alena Provotorova <lunichkina.alena@gmail.com>
2025-07-23 10:53:17 +0300

    netlify2

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/alenaprovotorova/flutter_projects/photo_app_client/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/alenaprovotorova/flutter_projects/photo_app_client/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/screen_retriever_macos.build/Objects-normal/x86_64/screen_retriever_macos_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/alenaprovotorova/flutter_projects/photo_app_client/build/macos/Build/Intermediates.noindex/Pods.build/Debug/screen_retriever_macos.build/module.modulemap \
+  /Users/alenaprovotorova/flutter_projects/photo_app_client/build/macos/Build/Intermediates.noindex/Pods.build/Debug/screen_retriever_macos.build/DerivedSources/screen_retriever_macos_vers.c
